215d45bd6130bc95ea4b23c332db56615382a5d8
mspaprika <arunepaprika@gmail.com>
2023-06-23 18:50:17 +0100

    register login and logout functionality added

---
+++ router.mjs
@@ -0,0 +1,15 @@
+import express from 'express';
+import { config } from 'dotenv';
+import { getUsers } from './database.mjs';
+
+config();
+
+const router = express.Router();
+
+router.get('/users', async (req, res) => {
+    const users = await getUsers();
+    res.send(users);
+});
+
+
+export default router;
